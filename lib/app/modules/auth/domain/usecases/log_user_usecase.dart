46388e1b3e7033d1c350a15d3d199eb95eb36049
jozadaque <jozadaquesousa@gmail.com>
2022-09-11 23:25:41 -0300

    feat: :sparkles: create usercase to log user

--- lib/app/modules/auth/domain/usecases/log_user_usecase.dart
+++ lib/app/modules/auth/domain/usecases/log_user_usecase.dart
@@ -1,21 +1,43 @@
 import 'package:dartz/dartz.dart';
+import 'package:glpi_hoiol/app/modules/auth/domain/repositories/i_log_user_repository.dart';
 import '../types/params.dart';
 
 abstract class ILogUserUsecase {
   Future<Either<Exception, String>> login(Params params);
-  Future<Either<Exception, bool>> logout();
+  Future<Either<Exception, Unit>> logout(String authToken);
 }
 
 class LogUserUsecase extends ILogUserUsecase {
+  final IlogUserRepository repository;
+
+  LogUserUsecase(this.repository);
+
   @override
-  Future<Either<Exception, bool>> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<Either<Exception, String>> login(Params params) async {
+    if (params.user.isEmpty) {
+      return left(Exception());
+    }
+
+    if (params.password.isEmpty) {
+      return left(Exception());
+    }
+
+    if (params.password.length < 4) {
+      return left(Exception());
+    }
+
+    final result = await repository.login(params);
+
+    return result;
   }
 
   @override
-  Future<Either<Exception, String>> login(Params params) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<Either<Exception, Unit>> logout(String authToken) async {
+    if (authToken.isEmpty) {
+      return left(Exception());
+    }
+
+    final result = await repository.logout(authToken);
+    return result;
   }
 }
