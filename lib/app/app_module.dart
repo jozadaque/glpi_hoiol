3a956a0fb2722654c600e3482108bc4f7f15daf2
jozadaque <jozadaquesousa@gmail.com>
2022-10-09 12:26:11 -0300

    feat: :fire: remove auth files

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'modules/tickets/ticket_module.dart';
+
 class AppModule extends Module {
   @override
   List<Bind> get binds => [];
--- lib/app/modules/auth/domain/types/params.dart
+++
@@ -1,9 +0,0 @@
-class Params {
-  final String password;
-  final String user;
-
-  Params({
-    required this.password,
-    required this.user,
-  });
-}
