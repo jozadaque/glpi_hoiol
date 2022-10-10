75401e77fa43631a312151f26bd0419b856b3138
jozadaque <jozadaquesousa@gmail.com>
2022-10-10 00:00:30 -0300

    refactor:

--- lib/app/modules/tickets/infra/datasources/i_ticket_datasource.dart
+++ lib/app/modules/tickets/infra/datasources/i_ticket_datasource.dart
@@ -1,9 +1,9 @@
-import '../../domain/entity/itil_category_entity.dart';
-import '../../domain/entity/ticket_entity.dart';
+import '../entity/itil_categories.dart';
+import '../entity/ticket_entity.dart';
 
 abstract class ITicketDatasource {
-  Future<List<TicketEntity>> getTickets();
-  Future<TicketEntity> getTicketById(int id);
-  Future<List<ItilCategoryEntity>> getCategories();
-  Future<ItilCategoryEntity> getCategoriesById(int id);
+  Future<List<Ticket>> getTickets();
+  Future<Ticket> getTicketById(int id);
+  Future<List<ItilCategories>> getCategories();
+  Future<ItilCategories> getCategoriesById(int id);
 }
